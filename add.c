1cd427c7cbce742ec449580c0f11f75355f8f346
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2023-08-27 23:21:51 +0000

    Fix

--- add.c
+++ add.c
@@ -18,7 +18,7 @@ void f_add(stack_t **head, unsigned int counter)
   }
   if (len < 2)
   {
-    fprintf(stderr, "L%d: Error, the stack is too short\n", counter);
+    fprintf(stderr, "L%d: can't add, stack too short\n", counter);
     fclose(bus.file);
     free(bus.content);
     free_stack(*head);
