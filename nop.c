a72b5b6404ce6e7567a5956a0905d813b6262b66
OmphileGopane <omphilegopane913@gmail.com>
2023-08-27 12:53:35 -0700

    more files

---
+++ nop.c
@@ -0,0 +1,12 @@
+include "monty.h"
+/**
+  *f_nop- couner function
+  *@head: head
+  *@counter: counter
+  *Return: void
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
